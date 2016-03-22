5e462ecd12a4b67da49db53d71b879753e942175
mpgovinda <mp.govinda@gmail.com>
2016-03-22 12:16:00 +0530

    those are the script for laser gun and player got damage... check those

---
+++ script/LaserAttack.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserAttack : MonoBehaviour {
+	public float timeBetweenAttacks=0.5f;
+	public int attackDamage=10;
+	GameObject player;
+	PlayerHealth playerHealth;
+	bool playerInRange;
+	float timer;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent<PlayerHealth> ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject == player) {
+			playerInRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == player) {
+			playerInRange = false;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= timeBetweenAttacks && playerInRange) {
+			Attack (); 
+		}
+	}
+
+	void Attack()
+	{
+		timer = 0f;
+		if (playerHealth.currentHealth > 0) {
+			playerHealth.TakeDamage (attackDamage);
+		}
+	}
+}
---
+++ script/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerHealth : MonoBehaviour {
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color (1f, 0f, 0f, 0.1f);
+	bool damaged;
+	// Use this for initialization
+	void Start () {
+		currentHealth = startingHealth;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (damaged) {
+			damageImage.color = flashColor;
+		}
+		else {
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed*Time.deltaTime);
+		}
+		damaged = false;
+	}
+	public void TakeDamage(int amount)//other scripts call this function...
+	{
+		damaged = true;
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
+	}
+}
